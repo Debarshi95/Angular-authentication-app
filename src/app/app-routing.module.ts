433da715e51d43ec07f0428eece820841a6b0ed7
Debarshi Bhattacharjee <debarshib80@gmail.com>
2019-08-30 21:40:50 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EventsComponent } from "./components/events/events.component";
+import { SpecialEventsComponent } from "./components/special-events/special-events.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/events", pathMatch: "full" },
+  { path: "events", component: EventsComponent },
+  { path: "special", component: SpecialEventsComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
