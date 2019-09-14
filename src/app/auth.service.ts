e648aeba5e930f658703d28b07607e1a81fb89f2
Debarshi Bhattacharjee <debarshib80@gmail.com>
2019-09-14 18:30:49 +0530

    minor changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, pipe } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -8,13 +8,20 @@ import { map } from "rxjs/operators";
 })
 export class AuthService {
   private url = "http://localhost:8000/api";
+  httpOptions = new HttpHeaders({
+    "X-Requested-With": "XMLHttpRequest"
+  });
   constructor(private http: HttpClient) {}
 
   registerUser(user): Observable<any> {
-    return this.http.post<any>(`${this.url}/register`, user);
+    return this.http.post<any>(`${this.url}/register`, user, {
+      headers: this.httpOptions
+    });
   }
 
   loginUser(user): Observable<any> {
-    return this.http.post<any>(`${this.url}/login`, user);
+    return this.http.post<any>(`${this.url}/login`, user, {
+      headers: this.httpOptions
+    });
   }
 }
